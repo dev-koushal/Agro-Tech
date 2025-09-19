c0065848b87a7a3aea1ae6727d7a78da37794973
dev-koushal <koushalydv99@gmail.com>
2025-09-19 19:53:06 +0530

    intialize

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Hero() {
+  return (
+    <div>
+        <div className='Hero-section'>
+            <Navbar />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
